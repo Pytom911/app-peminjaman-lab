5343499bd34b0964e2b25e30cf4ec02d8587eb9c
TomCDX <tomipmgr@gmail.com>
2026-08-03 14:56:59 +0700

    Update versi 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
+import { useState } from 'react';
+
 function App() {
+  // Membuat state bernama 'angka' dengan nilai awal 0
+  const [angka, setAngka] = useState(0);
+
   return (
     <div>
-      <h1>Halo, ini React pertamaku!</h1>
+      <h1>Angka saat ini: {angka}</h1>
+      <button onClick={() => setAngka(angka + 1)}>Tambah (+)</button>
+      <button onClick={() => setAngka(angka - 1)}>Kurang (-)</button>
     </div>
   );
 }
